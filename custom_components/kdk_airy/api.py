819d56d16c501666a5067eb8eca548cdf59469f9
bunbury86 <61380928+bunbury86@users.noreply.github.com>
2026-03-23 20:12:09 +0800

    Additional device and bug for Home Assistant state (#7)
    * Update api.py
    
    Added 48INCH-LED device (tested)
    
    * Update fan.py
    
    Correct status update in Home Assistant: if percentage is valued, home assistant will show as on.
    Tested and working
    
    * Update fan.py
    
    Added another bug fix for the on/off correct value in home assistant
    
    * Update fan.py

--- custom_components/kdk_airy/api.py
+++ custom_components/kdk_airy/api.py
@@ -82,7 +82,7 @@ class KdkDevice(NamedTuple):
     @property
     def has_lights(self):
         "Whether the fan device also has lights."
-        return self.product_code in ["E48GP"]
+        return self.product_code in ["E48GP", "48INCH-LED"]
 
 
 @dataclass
@@ -226,6 +226,7 @@ class KdkApiClient:
     SUPPORTED_PRODUCT_CODES = [
         "E48HP",
         "E48GP",
+        "48INCH-LED",
     ]
 
     # Hard-coded values from the KDK Ceiling Fan app v1.1.0
@@ -501,6 +502,7 @@ class KdkApiClient:
             get_status_packet = {
                 "E48HP": "0A00800000F00000860000880000F80000F20000F10000F90000FA0000FB00",
                 "E48GP": "0F00800000F00000860000880000F80000F20000F10000F90000FA0000FB0000F30000F50000F40000F70000F600",
+                "48INCH-LED": "0F00800000F00000860000880000F80000F20000F10000F90000FA0000FB0000F30000F50000F40000F70000F600",
             }[device.product_code]
 
             command_request_id = await self.send_command(
